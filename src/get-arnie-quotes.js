93f0bf4571414fb1631de253422ece77a14a2851
Nasim Mehri <nasimmehri@Nasims-MacBook-Pro.local>
2026-05-20 17:36:38 +1000

    implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,11 +1,25 @@
 const { httpGet } = require('./mock-http-interface');
 
 /**
+ * Fetches Arnie quotes from multiple URLs in parallel and returns
+ * either "Arnie Quote" or "FAILURE" based on HTTP status.
+ *
  * @param {GetArnieQuotesInput} urls
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+   return Promise.all(
+    urls.map(async (url) => {
+      const response = await httpGet(url);
+      const body = JSON.parse(response.body);
+
+      if (response.status === 200) {
+        return { 'Arnie Quote': body.message };
+      }
+
+      return { FAILURE: body.message };
+    })
+  );
 };
 
 module.exports = {
